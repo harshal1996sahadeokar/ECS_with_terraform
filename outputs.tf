dc088ce271a76a66c8ac20e173be0711a1c03110
HARSHAL SAHADEOKAR <131584127+harshal1996sahadeokar@users.noreply.github.com>
2025-02-15 22:07:18 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+# 🔹 Output ALB DNS Name & ECS Task Info
+output "alb_dns" {
+  description = "ALB DNS Name"
+  value       = aws_lb.nginx_alb.dns_name
+}
+
+output "ecs_task_def" {
+  description = "ECS Task Definition ARN"
+  value       = aws_ecs_task_definition.nginx_task.arn
+}
 
